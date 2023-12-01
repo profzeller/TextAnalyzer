8479895e31f9d1316910a2f4602e6b1cd7dbc00b
Th3Prim3 <45297253+Th3Prim3@users.noreply.github.com>
2023-12-01 16:29:24 -0600

    add requirements

--- submission/tasks.py
+++ submission/tasks.py
@@ -30,10 +30,19 @@ def fetch_openai_scores(submission_id, text):
 
     # Assuming you want to get scores from two different models, GPT-3.5 and GPT-4
     try:
-        response_3_5 = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=prompt,
-            max_tokens=50
+        response_3_5 = openai.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {
+                    "role": "system",
+                    "content": "Analyze the following text for its readability and provide scores for standard readability tests like the SMOG Index, Flesch-Kincaid Grade Level, Coleman-Liau Index, and Gunning Fog Index. The output should be in the following json format:\n\n    {{\n    'smog_index' = 3.5,\n    'flesch_kincaid_grade' = 2.5,\n    'coleman_liau_index' = 3.5,\n    'gunning_fog' = 2.5\n    }}\n\n"
+                },
+                {
+                    "role": "user",
+                    "content": text,
+                }
+                ],
+            max_tokens=256
         )
         # response_4 = openai.Completion.create(
         #     model="text-davinci-004",  # Replace with actual GPT-4 model name if different
@@ -44,11 +53,12 @@ def fetch_openai_scores(submission_id, text):
         # Fetch the submission instance
         submission = TextSubmission.objects.get(id=submission_id)
 
-        for test, score in json.loads(response_3_5.choices[0].text).items():
+        for test, score in json.loads(response_3_5.choices[0].message['content']).items():
             # Update the submission with new scores
             submission.readability_scores[test].append({
                 'score': score,
-                'source': 'OpenAI GPT-3.5 text-davinci-003'
+                'source': 'OpenAI GPT-3.5 Turbo',
+                'usage': json.loads(response_3_5.usage),
             })
         # for test, score in json.loads(response_4.choices[0].text).items():
         #     submission.readability_scores[test].append({
@@ -65,8 +75,3 @@ def fetch_openai_scores(submission_id, text):
     except Exception as e:
         # Handle exceptions (e.g., API errors, submission not found)
         print(f"Error in fetching OpenAI scores: {e}")
-
-@shared_task
-def add(x, y):
-    logger.info("Adding %d + %d", x, y)
-    return x + y
